0bbeb17d9f5fde86c8e6bd0635100452c1847324
yasinarafat4870 <yasinarafat4870@gmail.com>
2024-01-12 18:46:52 +0600

    Js half done

---
+++ main.js
@@ -0,0 +1,17 @@
+// Countdown
+
+const counter = document.querySelectorAll(".counter");
+const btn = document.querySelector(".btn");
+let counterArr = Array.from(counter);
+
+btn.addEventListener("click", function () {
+  counterArr.map((e) => {
+    let counterValue = e.innerHTML;
+    let count = 0;
+
+    function counterUp() {
+      e.innerHTML = count;
+      count++;
+    }
+  });
+});
